ecab27081ffd84a93f9b0a2800d64fe01fa9cc76
9834mean <9834min@mediasol.kr>
2019-05-31 14:36:53 +0900

    베이스 업로드

---
+++ WPF공부/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WPF공부
+{
+    /// <summary>
+    /// MainWindow.xaml에 
+    /// 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void OpenInfoPage(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("info.xaml", UriKind.Relative);
+        }
+
+    }
+}
